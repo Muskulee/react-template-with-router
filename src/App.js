1b2c2edfe8359598fd4b708c61ee6bc9efe0a114
Muskulee <muskulee@gmail.com>
2022-10-19 00:21:30 +0200

    Added React Router and Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import SearchForm from "./components/SearchForm";
 
-function App() {
+export default function App() {
+  const [closedNav, setClosedNav] = useState(false);
+
+  const toggleNav = () => {
+    setClosedNav(!closedNav);
+  };
+
+  const getNavWidth = () => (closedNav ? "w-16" : "w-56");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="flex">
+        {/* nav section */}
+
+        <div
+          className={
+            getNavWidth() +
+            " min-h-screen bg-red-100 transition-width border border-right"
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="sticky top-0">
+            <Navbar closed={closedNav} setLogOut={() => {}} />
+          </div>
+        </div>
+
+        {/* content section */}
+
+        <div className="flex-1 min-h-screen bg-gray-100">
+          <div className="sticky top-0">
+            <div className="flex item-center p-2 space-x-2">
+              <button onClick={toggleNav}>
+                {closedNav ? (
+                  <AiOutlineMenuUnfold size="25" />
+                ) : (
+                  <AiOutlineMenuFold size="25" />
+                )}
+              </button>
+
+              <SearchForm />
+            </div>
+          </div>
+
+          {/* Create The Routes Here */}
+          <div className="max-w-screen-lg mx-auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+              {/* <Route path="*" element={<Demo />} /> */}
+            </Routes>
+          </div>
+        </div>
+      </div>
+      {/* <Login /> */}
+    </>
   );
 }
-
-export default App;
